f607e180dc2d396f356d3263f660e1a9e14303f7
Beckster6211 <beckyljones151192@gmail.com>
2023-08-22 12:50:20 +0100

    created try page branch, created folders and files, copied code for try.js, done new background and emoji icons in page header

---
+++ src/components/Try/try.js
@@ -0,0 +1,204 @@
+import React, {useState, useEffect} from "react"
+import "./try.css"
+
+//components
+// import VisitTable from "./Table/table";
+// import VisitForm from "./Form/form";
+// import VisitLike from "./VisitHelper/like";
+// import VisitHelper from "./VisitHelper/visitHelper";
+// import VisitDislike from "./VisitHelper/dislike";
+
+// bootstrap
+import Accordion from 'react-bootstrap/Accordion';
+import Container from "react-bootstrap/Container"
+
+// firebase
+import {db} from "../../firebase"
+
+//firebase
+import {
+    query,
+    collection,
+    onSnapshot,
+    doc,
+    addDoc,
+    updateDoc,
+    deleteDoc
+} from "firebase/firestore"
+
+
+function Try({Becky}){
+    const [visit, setNewVisit] = useState([])
+    const [form, setForm] = useState({})
+    // console.log({visit})
+
+////add from form
+function handleChange(event){
+    setForm({...form, [event.target.id]:event.target.value})
+}
+
+
+// create
+async function handleSubmit(event){
+    console.log({form})
+    console.log("submit button pressed");
+event.preventDefault()
+await addDoc(collection(db, "Visit"),
+{
+    beckyopinion: "🤷‍♀️",
+    what: form.formWhat,
+    where: form.formWhere,
+    details: form.formVisitDescription,
+    recommendedby: form.formVisitRecommend,
+    stayed: false,
+});
+window.location.reload()
+}
+
+// read
+useEffect(()=>{
+    const q = query(collection(db, "Visit"))
+    const unsubscribe = onSnapshot(q, (querySnapshot)=>{ 
+        let visitArray = []
+        querySnapshot.forEach((doc)=>{
+            visitArray.push({...doc.data(),
+            id: doc.id})
+        })
+        setNewVisit(visitArray)
+    })
+    return () => unsubscribe()
+}, [])
+
+//update
+//tried
+const StayedThere = async (visit)=>{
+    await updateDoc(doc(db, "Visit", visit.id), {
+        stayed: !visit.stayed
+    })
+}
+
+//opinion
+const changeOpinion = async (read, event)=>{
+    let beckyOpinion = event.target.name;
+    await updateDoc(doc(db, "Visit", read.id),{
+        beckyopinion:beckyOpinion
+    })
+}
+
+//delete
+const deleteVisit = async(id)=>{
+    await deleteDoc(doc(db,"Visit", id))
+}
+
+return(
+        <Container 
+      fluid
+      className="tryPage pt-3"
+      style={{
+
+      }}
+      >
+      {/* <br/> */}
+      <Container 
+      id = "visitPage1"
+        className=" bg-secondary-subtle rounded text-center p-0" 
+        style={{
+          // fontSize:"3vw", 
+          // fontWeight:"900"
+        }}
+        >
+          🧗‍♀️ &nbsp; 🔫 &nbsp; Try PAGE &nbsp; 🛹 &nbsp; 🏄‍♀️
+      </Container>
+      {/* <br/> */}
+      <Container 
+      // fluid
+      className="d-flex justify-content-evenly py-2"
+      >
+          {/* <VisitLike/>
+          <VisitHelper/>
+          <VisitDislike/>  */}
+      </Container>
+      
+      {/* <br/> */}
+      <Accordion 
+      id="visitAccordion"
+      defaultActiveKey={["0"]} 
+      alwaysOpen
+      className="p-2 border border-dark border-3 rounded bg-light"
+      style={{
+      }}
+      >
+        <Accordion.Item 
+        eventKey="1" 
+        className="border border-dark border-3 rounded">
+          <Accordion.Header 
+          className="">
+            <Container 
+            // id="foodPage2"
+            fluid 
+            className="visitPageAccordion text-center"
+            style={{
+              // fontSize:"2.5vw", 
+              // fontWeight:"700"
+            }}
+            >
+              Try Form
+              </Container>
+           </Accordion.Header>
+          <Accordion.Body 
+          className="px-2 py-3">
+            {/* <div> */}
+                {/* film form */}
+              {/* <VisitForm
+              visit={visit}
+              handleChange={handleChange}
+            submitForm={handleSubmit}
+            /> */}
+            {/* </div> */}
+          </Accordion.Body>
+        </Accordion.Item>
+        {/* <br/> */}
+        <br/>
+        <Accordion.Item 
+        eventKey="0" 
+        className="border border-dark border-3 rounded"
+        // style={{height:"70vw"}}
+        >
+          <Accordion.Header 
+        //   className="bg-info"
+          >
+            <Container 
+            // id="foodPage3"
+            fluid 
+            className="visitPageAccordion text-center"
+            style={{
+              // fontSize:"2.5vw", 
+              // fontWeight:"700"
+            }}
+            >
+              Try Table
+            </Container>
+            </Accordion.Header>
+          <Accordion.Body 
+          className="px-2 py-3"
+          >
+            {/* <div>
+                Film Table */}
+              {/* <VisitTable
+              userBecky={Becky}
+              visit={visit}
+              stayedThere={StayedThere}
+              deleteVisit={deleteVisit}
+              handleOpinion={changeOpinion}
+              
+            /> */}
+            {/* </div> */}
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
+      <br/>
+      </Container>
+)
+}
+
+export default Try
--- src/components/dataFiles/dataTry.js
+++ src/components/dataFiles/dataTry.js
@@ -17,10 +17,29 @@ let tryThis = [
       visited: false,
       beckyopinion: "🤷‍♀️",
     },
+    {
+      what: "Combe Martin Wildlife & Dinosaur Park Park",
+      location: "Combe Martin, EX34 0NG, Devon",
+      why: "Lot of fun, interesting talks, I really enjoyed the talks especially the sealion and wolf ones, the dinosaurs are mischievous as well 🙂",
+      recommendedby: "Me (and my family)",
+      visited: true,
+      beckyopinion: "👍",
+    },
+    {
+      what: "Woodlands Family Theme Park",
+      location: "Woodlands Leisure Park, Totnes TQ9 7DQ Devon",
+      why: "SO MUCH FUN. Rides, water rides, animals, climbing courses, best to go on good weather days (there are lockers for spare clothes, when you get wet) 😀 https://www.woodlandspark.com/",
+      recommendedby: "Me (and my family)",
+      visited: true,
+      beckyopinion: "👍",
+    }
 
     //skateboarding
-    //shoot gun
+    //shoot gun, NPF Bassetts Pole, adventure park https://www.npfbassettspole.com/
     // throw out of plane
+    // camel trail padstow? chip shop (chicken nuggets)???
+    // fowey chip shop curry sauce
+    // cadbury world, its chocolate (free pot of chocolate yum yum)
   ];
   
   module.exports = tryThis;
